baa5fdcc30b35ff4f02bbb21dd4da614a74fcc78
Rokas <rookas.rudzenskas@gmail.com>
2021-05-01 13:56:05 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
+import firebase from "firebase";
 
-
-
-
-
+// credentials to login to the database
 const firebaseConfig = {
     apiKey: "AIzaSyCAnsfF_JOO70n6QiM4m6Cxy8NADsCfiwQ",
     authDomain: "tinder-clone-da3e2.firebaseapp.com",
@@ -12,3 +10,13 @@ const firebaseConfig = {
     appId: "1:230338843475:web:05241f3b2d653b4279c7a6",
     measurementId: "G-3YE7L64JGT"
 };
+
+// we connect to the database with credentials from firebaseconfig
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// we access the real time database
+const database = firebaseApp.firestore();
+
+
+// export to use it in all the app, whatever you want to use!
+export default database;
