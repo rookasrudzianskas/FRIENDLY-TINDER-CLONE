3c7f3dd8479382419e8efa5bdaba653d7a01d845
Rokas <rookas.rudzenskas@gmail.com>
2021-05-01 10:41:54 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello, Tinder Clone 🚀</h1>
 
-    {/*    Header   */}
+        {/*    Header   */}
         <Header />
-    {/*    end of Header*/}
 
-    {/*    Tinder Cards     */}
-    {/*    Buttons below the tinder cards    */}
+        <Router>
 
-    {/*    Chats screen, then we click on the top right icon    */}
-    {/*    Individual chat screen   */}
+
+            <Switch>
+
+                <Route path="/chat">
+                    <h1>I am chatpage</h1>
+                </Route>
+
+
+                <Route path="/">
+                    <h1>I am hompage</h1>
+                </Route>
+
+            </Switch>
+
+
+
+        </Router>
 
     </div>
   );
 }
 
 export default App;
+
+
+
+{/*    end of Header*/}
+
+{/*    Tinder Cards     */}
+{/*    Buttons below the tinder cards    */}
+
+{/*    Chats screen, then we click on the top right icon    */}
+{/*    Individual chat screen   */}
