97c79f242553f3e631eb09c7a90604c2f0ef8a32
Rokas <rookas.rudzenskas@gmail.com>
2021-05-01 11:34:55 +0300

    Roaks

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAnsfF_JOO70n6QiM4m6Cxy8NADsCfiwQ",
+    authDomain: "tinder-clone-da3e2.firebaseapp.com",
+    projectId: "tinder-clone-da3e2",
+    storageBucket: "tinder-clone-da3e2.appspot.com",
+    messagingSenderId: "230338843475",
+    appId: "1:230338843475:web:05241f3b2d653b4279c7a6",
+    measurementId: "G-3YE7L64JGT"
+};
