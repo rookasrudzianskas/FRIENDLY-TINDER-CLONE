bb161d94de68579e8599013aaf5b2cc73c0d2812
Rokas <rookas.rudzenskas@gmail.com>
2021-05-01 10:28:54 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
+
+// ES6 ARROW Functions
 const Header = () => {
     return (
-        <div>
-           <PersonIcon />
+        <div className="header">
+           <PersonIcon className="header__icon" fontSize="large"/>
+            <img className="header_logo" src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png" />
+            <ForumIcon className="header__icon" fontSize="large" />
         </div>
     )
 }
