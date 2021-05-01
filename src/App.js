8452bdf9854d4efcb69de76948e329cfd320dd9c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-01 10:03:25 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="app">
       <h1>Hello, Tinder Clone 🚀</h1>
+
+    {/*    Header   */}
+    {/*    Tinder Cards     */}
+    {/*    Buttons below the tinder cards    */}
+
+    {/*    Chats screen, then we click on the top right icon    */}
+    {/*    Individual chat screen   */}
+
     </div>
   );
 }
