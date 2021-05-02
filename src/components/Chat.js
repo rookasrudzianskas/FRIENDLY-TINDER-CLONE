34a65fa7cb21ae1f44b13ed4874f0fa965021ae8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 11:27:25 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Chat = () => {
+const Chat = ({ name, message, profilePic, timestamp }) => {
     return (
-        <div>
+        <div className="chat" >
 
         </div>
     )
