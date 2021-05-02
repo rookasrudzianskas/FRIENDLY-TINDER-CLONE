c20556c4e2db4c6efb10ed247c39d0fb7ad941cd
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 11:36:09 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,9 +5,14 @@ import Avatar from "@material-ui/core/Avatar";
 const Chat = ({ name, message, profilePic, timestamp }) => {
     return (
         <div className="chat" >
-            <Avatar className="chat__image" alt="name" src={profilePic} />
+            <Avatar className="chat__image" alt={name} src={profilePic} />
+            <div>
+                <h2>{name}</h2>
+                <p>{message}</p>
+            </div>
+            <p>{timestamp}</p>
         </div>
-    )
+    );
 }
 
 export default Chat;
