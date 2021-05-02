86c03ccd88761ca86783b51296b280b38edf3d88
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 11:38:04 +0300

    Rokas

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
-
+import "./styles/Chat.css";
 
 const Chat = ({ name, message, profilePic, timestamp }) => {
     return (
         <div className="chat" >
             <Avatar className="chat__image" alt={name} src={profilePic} />
-            <div>
+            <div className="chat__details">
                 <h2>{name}</h2>
                 <p>{message}</p>
             </div>
-            <p>{timestamp}</p>
+            <p className="chat__timestamp">{timestamp}</p>
         </div>
     );
 }
