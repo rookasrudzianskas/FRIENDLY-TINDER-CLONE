64b43e3fbb41c52dd823d8b7481de7c6f21c7da8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 09:05:29 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ function App() {
   return (
     <div className="app">
 
-        {/*    Header   */}
-        <Header />
-
         <Router>
-
+            {/*    Header   */}
+            <Header />
 
             <Switch>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
-import {IconButton} from "@material-ui/core";
+import {IconButton, Link} from "@material-ui/core";
 
 
 // ES6 ARROW Functions
@@ -13,9 +13,11 @@ const Header = () => {
                 <PersonIcon className="header__icon" fontSize="large"/>
             </IconButton>
             <img className="header_logo" src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png" />
+            <Link to="/chats">
             <IconButton>
                  <ForumIcon className="header__icon" fontSize="large" />
             </IconButton>
+            </Link>
         </div>
     )
 }
